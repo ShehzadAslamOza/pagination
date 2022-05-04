928498463d2c5f27b12fe24aa00ce30c9250fc5e
Shehzad Aslam Oza <shehzadaslamoza@gmail.com>
2022-05-04 15:44:14 +0500

    console based pagination working

--- index.js
+++ index.js
@@ -1,2 +1,55 @@
-let q = document.getElementById("header");
-q.style.color = "red";
+const state = {
+  url: "https://jsonplaceholder.typicode.com/comments/",
+  resultsOnEachPage: 10,
+  startIndex: 1,
+  endIndex: 10,
+  dataMap: new Map(),
+};
+
+function display() {
+  for (let i = state.startIndex; i <= state.endIndex; i++) {
+    console.log(state.dataMap.get(i));
+  }
+}
+
+function next() {
+  state.startIndex = state.startIndex + state.resultsOnEachPage;
+  state.endIndex = state.startIndex + state.resultsOnEachPage - 1;
+  getData();
+}
+
+function previous() {
+  state.startIndex = state.startIndex - state.resultsOnEachPage;
+  state.endIndex = state.endIndex - state.resultsOnEachPage;
+
+  if (state.startIndex < 1) {
+    state.startIndex = 1;
+    state.endIndex = state.resultsOnEachPage;
+  } else {
+    getData();
+  }
+}
+
+async function getData() {
+  for (let i = state.startIndex; i <= state.endIndex; i++) {
+    if (!state.dataMap.has(i)) {
+      await fetch(state.url + i)
+        .then((response) => response.json())
+        .then((json) => {
+          state.dataMap.set(i, json);
+        });
+    }
+  }
+
+  display();
+}
+
+getData();
+
+setTimeout(() => {
+  next();
+
+  setTimeout(previous, 2000);
+
+  console.log(state.dataMap);
+}, 2000);
