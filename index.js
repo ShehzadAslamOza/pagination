7b4ea9ea89ad4c1c278b15bf9396bcc51ea90acd
Shehzad Aslam Oza <shehzadaslamoza@gmail.com>
2022-05-05 16:30:46 +0500

    navigation buttons added, tables added and some improvements

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// State
 const state = {
   url: "https://jsonplaceholder.typicode.com/comments/",
   resultsOnEachPage: 10,
@@ -6,22 +7,50 @@ const state = {
   dataMap: new Map(),
 };
 
+// variables
+let nextBtn = document.getElementById("btn-next");
+let prevBtn = document.getElementById("btn-prev");
+let tbody = document.getElementById("tbody");
+let loading = document.getElementById("loading");
+
+// Event Listners
+nextBtn.addEventListener("click", next);
+prevBtn.addEventListener("click", previous);
+
+// Functions
+
+// Displays the data
 function display() {
+  tbody.innerHTML = "";
   for (let i = state.startIndex; i <= state.endIndex; i++) {
-    console.log(state.dataMap.get(i));
+    if (state.dataMap.has(i)) {
+      let data = state.dataMap.get(i);
+      console.log(data);
+      createRow(data.id, data.name, data.email, data.body);
+    }
   }
 }
 
+// Moves to next Page
 function next() {
   state.startIndex = state.startIndex + state.resultsOnEachPage;
   state.endIndex = state.startIndex + state.resultsOnEachPage - 1;
+
+  // max data count is 500
+  if (state.startIndex >= 499 || state.endIndex >= 499) {
+    state.endIndex = 499;
+    state.startIndex = 499 - state.resultsOnEachPage;
+  }
+
   getData();
 }
 
+// Moves to previous Page
 function previous() {
   state.startIndex = state.startIndex - state.resultsOnEachPage;
   state.endIndex = state.endIndex - state.resultsOnEachPage;
 
+  // index cant go below 1
   if (state.startIndex < 1) {
     state.startIndex = 1;
     state.endIndex = state.resultsOnEachPage;
@@ -30,13 +59,17 @@ function previous() {
   }
 }
 
+// Gets the data from the source
 async function getData() {
   for (let i = state.startIndex; i <= state.endIndex; i++) {
     if (!state.dataMap.has(i)) {
+      loading.style.display = "inline";
+
       await fetch(state.url + i)
         .then((response) => response.json())
         .then((json) => {
           state.dataMap.set(i, json);
+          loading.style.display = "none";
         });
     }
   }
@@ -44,12 +77,26 @@ async function getData() {
   display();
 }
 
+// Creates a new row in the table
+function createRow(id, name, email, body) {
+  let tr = document.createElement("tr");
+  let th = document.createElement("th");
+  let td1 = document.createElement("td");
+  let td2 = document.createElement("td");
+  let td3 = document.createElement("td");
+
+  th.textContent = id;
+  td1.textContent = name;
+  td2.textContent = email;
+  td3.textContent = body;
+
+  tr.appendChild(th);
+  tr.appendChild(td1);
+  tr.appendChild(td2);
+  tr.appendChild(td3);
+
+  let tbody = document.getElementById("tbody");
+  tbody.appendChild(tr);
+}
+
 getData();
-
-setTimeout(() => {
-  next();
-
-  setTimeout(previous, 2000);
-
-  console.log(state.dataMap);
-}, 2000);
